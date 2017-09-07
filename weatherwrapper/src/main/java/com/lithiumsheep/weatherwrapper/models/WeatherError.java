b8f01de33778005363287114bbca73b2a42db25e
lolsheeplol <jesse.elite@gmail.com>
2017-09-07 16:27:48 -0400

    WeatherCallback returns onFailure(Error error) instead of onFailure(Exception exception)

--- weatherwrapper/src/main/java/com/lithiumsheep/weatherwrapper/models/WeatherError.java
+++ weatherwrapper/src/main/java/com/lithiumsheep/weatherwrapper/models/WeatherError.java
@@ -1,11 +1,12 @@
 package com.lithiumsheep.weatherwrapper.models;
 
-import com.squareup.moshi.Json;
-
 public class WeatherError {
 
-    @Json(name = "message")
     private String message;
+
+    public WeatherError(String message) {
+        this.message = message;
+    }
 
     public String getMessage() {
         return message;
