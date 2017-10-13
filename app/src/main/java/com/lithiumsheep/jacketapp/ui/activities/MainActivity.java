c636a739b27ca2189791ae42d7380d7fbbe9a83b
lolsheeplol <jesse.elite@gmail.com>
2017-10-13 14:53:36 -0400

    Updated to as beta 7

--- app/src/main/java/com/lithiumsheep/jacketapp/ui/activities/MainActivity.java
+++ app/src/main/java/com/lithiumsheep/jacketapp/ui/activities/MainActivity.java
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.transition.TransitionManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
