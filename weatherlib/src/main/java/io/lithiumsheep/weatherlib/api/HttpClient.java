c291dff02a94750f37777d071c3e1766255ef48b
lolsheeplol <jesse.elite@gmail.com>
2017-11-30 17:37:12 -0500

    Starting the retrofit service

--- weatherlib/src/main/java/io/lithiumsheep/weatherlib/api/HttpClient.java
+++ weatherlib/src/main/java/io/lithiumsheep/weatherlib/api/HttpClient.java
@@ -1,5 +1,25 @@
 package io.lithiumsheep.weatherlib.api;
 
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.moshi.MoshiConverterFactory;
+
 public class HttpClient {
 
+    private static String baseUrl() {
+        return "https://api.openweathermap.org";
+    }
+
+    private static WeatherService service;
+    private static WeatherService getService() {
+        if (service == null) {
+            service = new Retrofit.Builder()
+                    .baseUrl(baseUrl())
+                    .addConverterFactory(MoshiConverterFactory.create())
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .build()
+                    .create(WeatherService.class);
+        }
+        return service;
+    }
 }
