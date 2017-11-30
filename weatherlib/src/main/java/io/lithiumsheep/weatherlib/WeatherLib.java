b36164e65e52312217620ae0d6521f9287253dd3
lolsheeplol <jesse.elite@gmail.com>
2017-11-30 17:44:56 -0500

    Added reactiveapp to use RxJava and more

---
+++ weatherlib/src/main/java/io/lithiumsheep/weatherlib/WeatherLib.java
@@ -0,0 +1,6 @@
+package io.lithiumsheep.weatherlib;
+
+public class WeatherLib {
+
+
+}
--- weatherlib/src/main/java/io/lithiumsheep/weatherlib/models/CurrentWeather.java
+++ weatherlib/src/main/java/io/lithiumsheep/weatherlib/models/CurrentWeather.java
@@ -2,4 +2,13 @@ package io.lithiumsheep.weatherlib.models;
 
 public class CurrentWeather {
 
+    private Coord coord;
+    //List<Weather>
+    private String base;
+    // temperatures
+    private Wind wind;
+    private Clouds clouds;
+
+    private String id;
+    private String name;
 }
