ef2e7d686d717cb11dd2cc969da3c2041a9d0141
lolsheeplol <jesse.elite@gmail.com>
2018-04-20 19:14:10 -0400

    Removed WeatherWrapper module

--- weatherwrapper/src/main/java/com/lithiumsheep/weatherwrapper/location/LocationCallback.java
+++
@@ -1,8 +0,0 @@
-package com.lithiumsheep.weatherwrapper.location;
-
-import android.location.Location;
-
-public abstract class LocationCallback {
-    public abstract void onError(Exception exception);
-    public abstract void onLocationSuccess(Location location);
-}
