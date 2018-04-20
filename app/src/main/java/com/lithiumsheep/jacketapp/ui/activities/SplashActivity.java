a2837d5912a9d11ecd96864a94240eb7a3dd0e65
lolsheeplol <jesse.elite@gmail.com>
2018-04-20 18:22:04 -0400

    Get SplashActivity working

--- app/src/main/java/com/lithiumsheep/jacketapp/ui/activities/SplashActivity.java
+++ app/src/main/java/com/lithiumsheep/jacketapp/ui/activities/SplashActivity.java
@@ -1,19 +1,17 @@
 package com.lithiumsheep.jacketapp.ui.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
-import com.lithiumsheep.jacketapp.R;
-
-import butterknife.ButterKnife;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-        ButterKnife.bind(this);
+        // theme set through SplashTheme in AndroidManifest
 
+        // just forward to MainActivity
+        startActivity(new Intent(this, MainActivity.class));
     }
 }
