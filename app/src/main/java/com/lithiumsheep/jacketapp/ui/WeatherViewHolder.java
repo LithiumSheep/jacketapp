8a7bd6e5248ef543d7169f2007e3e6b46483158a
lolsheeplol <jesse.elite@gmail.com>
2018-05-29 20:06:03 -0400

    get magic number jacket icon and text working

--- app/src/main/java/com/lithiumsheep/jacketapp/ui/WeatherViewHolder.java
+++ app/src/main/java/com/lithiumsheep/jacketapp/ui/WeatherViewHolder.java
@@ -1,12 +1,14 @@
 package com.lithiumsheep.jacketapp.ui;
 
 import android.app.Activity;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.lithiumsheep.jacketapp.R;
 import com.lithiumsheep.jacketapp.models.weather.CurrentWeather;
 import com.lithiumsheep.jacketapp.util.Converter;
 import com.lithiumsheep.jacketapp.util.TimeUtil;
+import com.squareup.picasso.Picasso;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -25,6 +27,10 @@ public class WeatherViewHolder {
     TextView maxTemp;
     @BindView(R.id.weather_text)
     TextView name;
+    @BindView(R.id.weather_icon)
+    ImageView icon;
+    @BindView(R.id.jacket_text)
+    TextView jacketText;
 
     public WeatherViewHolder(Activity target) {
         ButterKnife.bind(this, target);
@@ -37,6 +43,20 @@ public class WeatherViewHolder {
         minTemp.setText(String.valueOf(weather.getMetrics().getTempMin()));
         maxTemp.setText(String.valueOf(weather.getMetrics().getTempMax()));
         name.setText(weather.getWeather().get(0).getMain());
+
+        if (weather.getMetrics().getTemp() < 294.261) { // magic number for 70 F
+            // TODO: later pull this from analysis or Converter + preferences
+            Picasso.get()
+                    .load(R.drawable.jacket_red)
+                    .into(icon);
+
+            jacketText.setText("Put on a jacket.  It may be chilly out.");
+        } else {
+            Picasso.get()
+                    .load(R.drawable.weather_sun)
+                    .into(icon);
+            jacketText.setText("You probably won't need a jacket today");
+        }
     }
 
     public void clear() {
@@ -46,5 +66,7 @@ public class WeatherViewHolder {
         minTemp.setText(null);
         maxTemp.setText(null);
         name.setText(null);
+        icon.setImageBitmap(null);
+        jacketText.setText(null);
     }
 }
