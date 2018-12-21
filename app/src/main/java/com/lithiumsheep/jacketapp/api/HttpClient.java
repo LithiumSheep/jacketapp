8ce4a43f90fa53d2a9d3aedf04d8b72ef13a5194
lolsheeplol <jesse.elite@gmail.com>
2018-12-21 15:51:35 -0500

    Fix compile issue with HttpClient for removing cache

--- app/src/main/java/com/lithiumsheep/jacketapp/api/HttpClient.java
+++ app/src/main/java/com/lithiumsheep/jacketapp/api/HttpClient.java
@@ -1,17 +1,15 @@
 package com.lithiumsheep.jacketapp.api;
 
 import com.lithiumsheep.jacketapp.BuildConfig;
-import com.lithiumsheep.jacketapp.JacketApplication;
 
 import java.util.concurrent.TimeUnit;
 
-import okhttp3.Cache;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.moshi.MoshiConverterFactory;
 
-public class HttpClient {
+public final class HttpClient {
 
     private static HttpLoggingInterceptor basicLogger() {
         HttpLoggingInterceptor logger = new HttpLoggingInterceptor();
@@ -23,10 +21,7 @@ public class HttpClient {
     private static synchronized OkHttpClient getClient() {
         if (client == null) {
             client = new OkHttpClient.Builder()
-                    .connectTimeout(10, TimeUnit.SECONDS)
-                    .writeTimeout(10, TimeUnit.SECONDS)
-                    .readTimeout(30, TimeUnit.SECONDS)
-                    .cache(new Cache(JacketApplication.getDefaultCacheDir(), JacketApplication.getDefaultCacheSize()))
+                    .callTimeout(20, TimeUnit.SECONDS)
                     .addInterceptor(basicLogger())
                     .build();
         }
@@ -36,7 +31,6 @@ public class HttpClient {
     private static OpenWeatherService service;
     public static OpenWeatherService get() {
         if (service == null) {
-
             service = new Retrofit.Builder()
                     .baseUrl(OpenWeatherService.URL)
                     .addConverterFactory(MoshiConverterFactory.create())
